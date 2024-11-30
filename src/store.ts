33b88bc35dab8cf543e58efdbb4224c48f8b46b8
Abel <90abel.avr@gmail.com>
2024-11-30 02:52:37 -0300

    crud finished

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,48 @@
 import { create } from "zustand"
 import { v4 as uuidv4 } from "uuid"
 import { DraftPatient, Patient } from "./types"
+import { devtools } from "zustand/middleware"
 
 type PatientState = {
     patients: Patient[]
+    activeID: Patient["id"]
     addPatient: (data : DraftPatient) => void
+    deletePatient: (id : Patient["id"]) => void
+    getPatientById: (id : Patient["id"])=> void
+    updatedPatient: (data: DraftPatient) => void
+
 }
 const createPatient = (patient : DraftPatient) : Patient => {
     return {...patient, id: uuidv4() }
 }
-export const usePatientStore = create<PatientState>((set)=>({
-    patients: [],
-    
-    addPatient: (data)=>{
+export const usePatientStore = create<PatientState>()(
+    devtools((set)=>({
+        patients: [],
+        activeID:"",
+        addPatient: (data)=>{
 
-        const newPatient = createPatient(data) 
-        set((state)=>({
-            patients:[...state.patients, newPatient]
-        }))
-    }
-}))
+            const newPatient = createPatient(data) 
+            set((state)=>({
+                patients:[...state.patients, newPatient]
+            }))
+        },
+        deletePatient: (id) => {
+            set((state)=>({
+                patients: state.patients.filter(patient => patient.id !== id )
+            }));
+        },
+        getPatientById: (id) => {
+            set(()=>({
+                activeID: id
+            }))
+            
+        },
+        updatedPatient: (data) => {
+            set((state)=>({
+                patients: state.patients.map(patient => patient.id === state.activeID ? {id: state.activeID, ...data} : patient),
+                activeID:""
+            }))
+        }
+
+})))
 
